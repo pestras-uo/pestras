ae56712abb4584f81631d4b21ad2d4c4a5b21b3a
chadi7781 <troudishedy6@gmail.com>
2023-10-21 09:37:26 +0100

    fix #77 Job to check workflow steps exceeded days limit

--- apps/workflow-check/src/job.ts
+++ apps/workflow-check/src/job.ts
@@ -1,47 +1,415 @@
-import { MongoClient } from "mongodb";
-import config from "./config";
-import { DataStore, Workflow } from "@pestras/shared/data-model";
+import { MongoClient, Db } from 'mongodb';
+import config from './config';
+import {
+  DataStore,
+  RecordWorkflow,
+  RecordWorkflowState,
+  TableDataRecord,
+  UserWorkflowAction,
+  Workflow,
+} from '@pestras/shared/data-model';
 
+//  Create a Map to store cached data---Optimization
+const dataCache: Map<string, RecordWorkflowState[]> = new Map();
+
+/**
+ * Function to fetch record workflow data from the database with caching.
+ * @param dataDb - Database connection object
+ * @param dsSerial - Data store serial number
+ */
+export async function getRecordWorkflow(dataDb: Db, dsSerial: string) {
+  // Check if data is in the cache
+  const cacheKey = `workflow_${dsSerial}`;
+  if (dataCache.has(cacheKey)) {
+    return dataCache.get(cacheKey);
+  }
+
+  // If data is not in the cache, fetch it from the database
+  const workflowData = await dataDb
+    .collection<RecordWorkflowState>(cacheKey)
+    .find({})
+    .toArray();
+
+  // Store data in the cache for future use
+  dataCache.set(cacheKey, workflowData);
+
+  return workflowData;
+}
+
+/**
+ * Main job function to process active workflows.
+ * @param conn - MongoDB client connection
+ */
 export async function job(conn: MongoClient) {
-  // your code
   const sysDb = conn.db(config.sysDb);
   const dataDb = conn.db(config.dataDb);
+  const date = new Date();
 
+  // Fetch data stores with activated workflows
+  const dataStores = await sysDb
+    .collection<DataStore>('data_stores')
+    .find(
+      {
+        $or: [
+          { 'settings.workflow.create': { $type: 'string' } },
+          { 'settings.workflow.update': { $type: 'string' } },
+          { 'settings.workflow.delete': { $type: 'string' } },
+        ],
+      },
+      { projection: { serial: 1, settings: 1 } }
+    )
+    .toArray();
 
-  // step 1: fetch data stores with workflow activated
-  const dataStores = await sysDb.collection<DataStore>("data-stores").find({
-    $or: [
-      { 'settings.workflow.create': { $type: 'string' } },
-      { 'settings.workflow.update': { $type: 'string' } },
-      { 'settings.workflow.delete': { $type: 'string' } }
-    ]
-  }, { projection: { serial: 1, settings: 1 } }).toArray();
-
+  // Iterate through each data store with an activated workflow
   for (const ds of dataStores) {
-    const wfSerial = Object.keys(ds.settings.workflow).reduce(((wf, action) => typeof wf === 'string' ? wf : action), '');
-    const workflowDefinition = await sysDb.collection<Workflow>("workflows").findOne({ serial: wfSerial });
+    // Fetch active workflows for the current data store
+    const activeWorkflows = await getRecordWorkflow(dataDb, ds.serial);
 
-    if (!workflowDefinition)
-      continue;
+    // Iterate through active workflows
+    for (const aw of activeWorkflows) {
+      const stateReview = aw.workflows.find((w) => w.state === 'review');
+      if (!stateReview) {
+        continue;
+      }
 
-    // |  - fetch all active steps
-    // |  - for each active step
-    // |  |  - check if step exceeded days limit
-    // |  |  - apply default action for each user
-    // |  |  |  case 1: state is rejection     
-    // |  |  |  |  case 1: action was delete     
-    // |  |  |  |  |  - move record from review table to approve table 
-    // |  |  |  |  case 2: else     
-    // |  |  |  |  |  - move record from review table to draft table 
-    // |  |  |  |  - delete all workflow steps 
-    // |  |  |  case 1: state is approval
-    // |  |  |  |  case 1: is last step
-    // |  |  |  |  |  case 1: action was delete
-    // |  |  |  |  |  |  - remove record from review and approve tables
-    // |  |  |  |  |  case 2: else
-    // |  |  |  |  |  |  - move record from review table to approve tables
-    // |  |  |  |  case 2: not last step
-    // |  |  |  |  |  - add new step with default state to record woekflow.
+      // Fetch workflow definition based on serial
+      const workflowDefinition = await sysDb
+        .collection<Workflow>('workflows')
+        .findOne({ serial: stateReview.workflow });
 
+      if (!workflowDefinition) continue;
+
+      // Find the current step index in the workflow
+      const currentStepIndex = stateReview.steps.findIndex(
+        (step) => step.state === 'review'
+      );
+      const reviewStep = stateReview.steps[currentStepIndex];
+
+      // Calculate days difference between current date and step start date
+      const currentDate = new Date();
+      const daysDifference = Math.floor(
+        (currentDate.getTime() - reviewStep.start_date.getTime()) /
+          (1000 * 60 * 60 * 24)
+      );
+
+      // Fetch step definition based on step serial
+      const stepDefinition = workflowDefinition.steps.find(
+        (step) => step.serial === reviewStep.step
+      );
+
+      if (!stepDefinition) continue;
+
+      // Check if the current step is the last step in the workflow
+      const isLastStep =
+        currentStepIndex === workflowDefinition.steps.length - 1;
+
+      // Perform actions based on workflow settings and user action
+      if (daysDifference > stepDefinition.max_review_days) {
+        const userAction = stateReview.trigger;
+
+        switch (stepDefinition.default_action) {
+          case 'reject':
+            // Update workflow step state to 'reject'
+            await updateWorkflowStep(
+              dataDb,
+              ds.serial,
+              aw.record,
+              reviewStep.step,
+              reviewStep.actions,
+              date,
+              'reject'
+            );
+
+            // Perform action based on user action
+            switch (userAction) {
+              case 'delete':
+                await moveRecordFromReviewToApprovalTable(
+                  dataDb,
+                  ds.serial,
+                  aw.record,
+                  reviewStep.step,
+                  reviewStep.actions,
+                  date
+                );
+                break;
+              default:
+                await moveRecordFromReviewToDraftTable(
+                  dataDb,
+                  ds.serial,
+                  aw.record
+                );
+                break;
+            }
+            break;
+          case 'approve':
+            if (isLastStep) {
+              // Remove record from review and approve tables based on user action
+              switch (userAction) {
+                case 'delete':
+                  await removeRecordFromReviewAndApproveTables(
+                    dataDb,
+                    ds.serial,
+                    aw.record
+                  );
+                  break;
+                default:
+                  await moveRecordToApproveTable(
+                    dataDb,
+                    ds.serial,
+                    aw.record,
+                    reviewStep.step,
+                    reviewStep.actions,
+                    date
+                  );
+                  break;
+              }
+            } else {
+              // Add a new step with default state to the record workflow
+              const nextStepDefinition =
+                workflowDefinition.steps[currentStepIndex + 1];
+              await addNewStepWithDefaultState(
+                dataDb,
+                ds.serial,
+                aw.record,
+                nextStepDefinition.serial,
+                reviewStep.step,
+                reviewStep.actions,
+                date
+              );
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  // Clear the cache after processing to free up memory
+  dataCache.clear();
+}
+
+/**
+ * Function to update workflow step state and actions.
+ * @param db - Database connection object
+ * @param dsSerial - Data store serial number
+ * @param record - Record identifier
+ * @param stepSerial - Step serial number
+ * @param actions - User workflow actions
+ * @param date - Date object
+ * @param state - New state for the workflow step
+ */
+async function updateWorkflowStep(
+  db: Db,
+  dsSerial: string,
+  record: string,
+  stepSerial: string,
+  actions: UserWorkflowAction[],
+  date: Date,
+  state: string
+) {
+  await db.collection<RecordWorkflowState>(`workflow_${dsSerial}`).updateOne(
+    { record: record, 'workflows.serial': dsSerial },
+    {
+      $set: {
+        'workflows.$.end_date': date,
+        'workflows.$.state': state,
+        'workflows.$.steps.$.actions': actions,
+        'workflows.$.steps.$[step].end_date': date,
+        'workflows.$.steps.$[step].state': state,
+      },
+    },
+    { arrayFilters: [{ 'step.step': stepSerial }] }
+  );
+}
+/**
+ * Function to move a record from the review table to the approval table.
+ * @param db - Database connection object
+ * @param dsSerial - Data store serial number
+ * @param record - Record identifier
+ * @param stepSerial - Step serial number
+ * @param actions - User workflow actions
+ * @param date - Date object
+ */
+async function moveRecordFromReviewToApprovalTable(
+  db: Db,
+  dsSerial: string,
+  record: string,
+  stepSerial: string,
+  actions: UserWorkflowAction[],
+  date: Date
+) {
+  // Update workflow step state to 'reject'
+  await updateWorkflowStep(
+    db,
+    dsSerial,
+    record,
+    stepSerial,
+    actions,
+    date,
+    'reject'
+  );
+}
+
+/**
+ * Function to move a record from the review table to the draft table.
+ * @param db - Database connection object
+ * @param dsSerial - Data store serial number
+ * @param recSerial - Record serial number
+ */
+async function moveRecordFromReviewToDraftTable(
+  db: Db,
+  dsSerial: string,
+  recSerial: string
+) {
+  // Fetch the record from the review table
+  const record = await db
+    .collection<TableDataRecord>(`review_${dsSerial}`)
+    .findOne({ serial: recSerial });
+
+  // If the record is found, move it to the draft table
+  if (record) {
+    await db.collection<TableDataRecord>(`draft_${dsSerial}`).insertOne(record);
+    await db
+      .collection<TableDataRecord>(`review_${dsSerial}`)
+      .deleteOne({ serial: recSerial });
+  }
+}
+
+/**
+ * Function to move a record to the approval table.
+ * @param db - Database connection object
+ * @param dsSerial - Data store serial number
+ * @param record - Record identifier
+ * @param stepSerial - Step serial number
+ * @param actions - User workflow actions
+ * @param date - Date object
+ */
+async function moveRecordToApproveTable(
+  db: Db,
+  dsSerial: string,
+  record: string,
+  stepSerial: string,
+  actions: UserWorkflowAction[],
+  date: Date
+) {
+  try {
+    const result = await db.collection(`workflow_${dsSerial}`).updateOne(
+      {
+        record: record,
+        'workflows.serial': dsSerial,
+      },
+      {
+        $set: {
+          'workflows.$.steps.$[step].actions': actions,
+          'workflows.$.steps.$[step].end_date': date,
+          'workflows.$.steps.$[step].state': 'approve',
+        },
+      },
+      {
+        arrayFilters: [{ 'step.step': stepSerial }],
+      }
+    );
+
+    if (result.modifiedCount === 1) {
+      console.log('Record moved to the approve table successfully.');
+    } else {
+      console.log('Failed to move record to the approve table.');
+    }
+  } catch (error) {
+    console.error('Error moving record to the approve table:', error);
+  }
+}
+
+/**
+ * Function to remove a record from the review and approve tables.
+ * @param db - Database connection object
+ * @param dsSerial - Data store serial number
+ * @param record - Record
+ */
+async function removeRecordFromReviewAndApproveTables(
+  db: Db,
+  dsSerial: string,
+  record: string
+) {
+  try {
+    const reviewResult = await db
+      .collection(`workflow_${dsSerial}`)
+      .findOneAndDelete({
+        record: record,
+        'workflows.serial': dsSerial,
+        'workflows.steps.state': 'review',
+      });
+
+    const approveResult = await db
+      .collection(`workflow_${dsSerial}`)
+      .findOneAndDelete({
+        record: record,
+        'workflows.serial': dsSerial,
+        'workflows.steps.state': 'approve',
+      });
+
+    if (reviewResult.ok === 1 && approveResult.ok === 1) {
+      console.log(
+        'Record removed from review and approve tables successfully.'
+      );
+    } else {
+      console.log('Failed to remove record from review and approve tables.');
+    }
+  } catch (error) {
+    console.error(
+      'Error removing record from review and approve tables:',
+      error
+    );
+  }
+}
+
+/**
+ * Function to add a new step with default state to the record workflow.
+ * @param db - Database connection object
+ * @param dsSerial - Data store serial number
+ * @param record - Record identifier
+ * @param stepSerial - Step serial number
+ * @param defaultState - Review step
+ * @param date - Date object
+ */
+async function addNewStepWithDefaultState(
+  db: Db,
+  dsSerial: string,
+  record: string,
+  stepSerial: string,
+  defaultState: string,
+  actions: UserWorkflowAction[],
+
+  date: Date
+) {
+  try {
+    const newStep = {
+      step: stepSerial,
+      state: defaultState,
+      start_date: date,
+      end_date: null,
+      actions: actions,
+    };
+
+    const result = await db.collection(`workflow_${dsSerial}`).updateOne(
+      {
+        record: record,
+        'workflows.serial': dsSerial,
+      },
+      {
+        $push: {
+          'workflows.$.steps': newStep,
+        },
+      }
+    );
+
+    if (result.modifiedCount === 1) {
+      console.log('New step added to the record workflow successfully.');
+    } else {
+      console.log('Failed to add a new step to the record workflow.');
+    }
+  } catch (error) {
+    console.error('Error adding a new step to the record workflow:', error);
   }
 }
--- libs/backend/models/src/lib/blueprints/record-workflow/approve.ts
+++ libs/backend/models/src/lib/blueprints/record-workflow/approve.ts
@@ -1,7 +1,13 @@
-import { RecordWorkflowModel } from ".";
-import { HttpError, HttpCode } from "@pestras/backend/util";
-import { dataRecordsModel, dataStoresModel, workflowModel } from "../../models";
-import { DataRecordState, RecordWorkflow, TableDataRecord, User, getWorkflowStepAction } from "@pestras/shared/data-model";
+import { RecordWorkflowModel } from '.';
+import { HttpError, HttpCode } from '@pestras/backend/util';
+import { dataRecordsModel, dataStoresModel, workflowModel } from '../../models';
+import {
+  DataRecordState,
+  RecordWorkflow,
+  TableDataRecord,
+  User,
+  getWorkflowStepAction,
+} from '@pestras/shared/data-model';
 
 export async function approve(
   this: RecordWorkflowModel,
@@ -13,64 +19,67 @@ export async function approve(
 ): Promise<DataRecordState | null> {
   const date = new Date();
   const activeWf = await this.getRecordActiveWf(dsSerial, recSerial);
-  const recordWorkFlowCol = this.db.collection<RecordWorkflow>(`workflow_${dsSerial}`);
+  const recordWorkFlowCol = this.db.collection<RecordWorkflow>(
+    `workflow_${dsSerial}`
+  );
+  
 
   if (!activeWf)
     throw new HttpError(HttpCode.NOT_FOUND, 'recordWorkflowNotFound');
 
-  const wfStep = activeWf.steps.find(s => s.step === step);
+  const wfStep = activeWf.steps.find((s) => s.step === step);
 
   if (!wfStep)
     throw new HttpError(HttpCode.NOT_FOUND, 'recordWorkflowStepNotFound');
 
   const wf = await workflowModel.getBySerial(activeWf.workflow);
 
-  if (!wf)
-    throw new HttpError(HttpCode.NOT_FOUND, 'workflowNotFound');
+  if (!wf) throw new HttpError(HttpCode.NOT_FOUND, 'workflowNotFound');
 
-  const currStepIndex = wf.steps.findIndex(p => p.serial === step);
+  const currStepIndex = wf.steps.findIndex((p) => p.serial === step);
 
   if (currStepIndex === -1)
     throw new HttpError(HttpCode.EXPECTATION_FAILED, 'workflowStepDefNotFound');
 
   const wfStepOpt = wf.steps[currStepIndex];
 
-  wfStep.actions = wfStep.actions.map(a =>
-    a.user === issuer.serial
-      ? { ...a, action: 'approve', date, message }
-      : a
+  wfStep.actions = wfStep.actions.map((a) =>
+    a.user === issuer.serial ? { ...a, action: 'approve', date, message } : a
   );
 
   const stepState = getWorkflowStepAction(wfStep.actions, wfStepOpt.algo);
 
   if (stepState === 'approve') {
-
     // is last step
     if (currStepIndex === wf.steps.length - 1) {
       const ds = await dataStoresModel.getBySerial(dsSerial, { settings: 1 });
 
-      if (!ds)
-        throw new HttpError(HttpCode.NOT_FOUND, 'dataStoreNotFound');
+      if (!ds) throw new HttpError(HttpCode.NOT_FOUND, 'dataStoreNotFound');
 
       const mainCol = this.db.collection<TableDataRecord>(dsSerial);
-      const reviewCol = this.db.collection<TableDataRecord>(`review_${dsSerial}`);
-      const record = await reviewCol.findOne({ serial: recSerial }, { projection: { _id: 0 } });
+      const reviewCol = this.db.collection<TableDataRecord>(
+        `review_${dsSerial}`
+      );
+      const record = await reviewCol.findOne(
+        { serial: recSerial },
+        { projection: { _id: 0 } }
+      );
 
-      if (!record)
-        throw new HttpError(HttpCode.NOT_FOUND, 'recordNotFound');
+      if (!record) throw new HttpError(HttpCode.NOT_FOUND, 'recordNotFound');
 
-      // push current state to history 
+      // push current state to history
       if (ds.settings.history) {
         const currState = await mainCol.findOne({ serial: recSerial });
 
-        if (currState)
-          dataRecordsModel.pushHistory(dsSerial, currState);
+        if (currState) dataRecordsModel.pushHistory(dsSerial, currState);
       }
 
       if (activeWf.trigger === 'delete')
         await mainCol.deleteOne({ serial: recSerial });
       else
-        await mainCol.replaceOne({ serial: recSerial }, record, { upsert: true });
+        await mainCol.replaceOne({ serial: recSerial }, record, {
+          upsert: true,
+        });
 
       await reviewCol.deleteOne({ serial: recSerial });
       await recordWorkFlowCol.updateOne(
@@ -81,14 +90,13 @@ export async function approve(
             'workflows.$.state': 'approve',
             'workflows.$.steps.$[step].actions': wfStep.actions,
             'workflows.$.steps.$[step].end_date': date,
-            'workflows.$.steps.$[step].state': 'approve'
-          }
+            'workflows.$.steps.$[step].state': 'approve',
+          },
         },
         { arrayFilters: [{ 'step.step': step }] }
       );
 
       return activeWf.trigger === 'delete' ? null : 'published';
-
     } else {
       const nextStep = wf.steps[currStepIndex + 1];
 
@@ -98,8 +106,8 @@ export async function approve(
           $set: {
             'workflows.$.steps.$[step].actions': wfStep.actions,
             'workflows.$.steps.$[step].end_date': date,
-            'workflows.$.steps.$[step].state': 'approve'
-          }
+            'workflows.$.steps.$[step].state': 'approve',
+          },
         },
         { arrayFilters: [{ 'step.step': step }] }
       );
@@ -108,24 +116,32 @@ export async function approve(
         {
           $push: {
             'workflows.$.steps': {
-              actions: nextStep.users.map(u => ({ user: u, action: 'review', date: null, message: '' })),
+              actions: nextStep.users.map((u) => ({
+                user: u,
+                action: 'review',
+                date: null,
+                message: '',
+              })),
               step: nextStep.serial,
               state: 'review',
               start_date: date,
-              end_date: null
-            }
-          }
-        });
+              end_date: null,
+            },
+          },
+        }
+      );
 
       return 'review';
     }
   }
 
-  await this.db.collection(`workflow_${dsSerial}`).updateOne(
-    { record: recSerial, 'workflows.serial': activeWf.serial },
-    { $set: { 'workflows.$.steps.$[step].actions': wfStep.actions } },
-    { arrayFilters: [{ 'step.step': step }] }
-  );
+  await this.db
+    .collection(`workflow_${dsSerial}`)
+    .updateOne(
+      { record: recSerial, 'workflows.serial': activeWf.serial },
+      { $set: { 'workflows.$.steps.$[step].actions': wfStep.actions } },
+      { arrayFilters: [{ 'step.step': step }] }
+    );
 
   return 'review';
 }
