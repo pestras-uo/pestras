2ba9c495ed26f7fbfeb31f910cb27e3375b22a5e
chadi7781 <troudishedy6@gmail.com>
2023-11-24 09:22:20 +0100

    fixes : #98 Optimizing charts data consumption remove unsed code

--- apps/platform/src/app/main/workspace/views/slides/slides.view.ts
+++ apps/platform/src/app/main/workspace/views/slides/slides.view.ts
@@ -11,7 +11,6 @@ import {
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import {
-  DataStore,
   Workspace,
   WorkspaceDashboardSlide,
 } from '@pestras/shared/data-model';
